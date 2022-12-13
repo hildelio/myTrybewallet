bef4a02775ca60979ba7e4a4851c2bf301b152be
hildelio <hildelio@gmail.com>
2022-12-12 21:40:29 -0300

    Requisito 5 :heavy_check_mark:

---
+++ src/tests/login.test.js
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+
+describe('Verificar se na página login...', () => {
+  test('Tem um input de email com data testid = email-input', () => {
+    renderWithRouterAndRedux(<App />);
+    const inputEmailEl = screen.getByTestId('email-input');
+    expect(inputEmailEl).toBeInTheDocument();
+  });
+  test('Tem um input de password com data testid = password-input', () => {
+    renderWithRouterAndRedux(<App />);
+    const inputPasswordEl = screen.getByTestId('password-input');
+    expect(inputPasswordEl).toBeInTheDocument();
+  });
+  test('Tem um botão para entrar na aplicação', () => {
+    renderWithRouterAndRedux(<App />);
+    const inputButtonEl = screen.getByRole('button', { name: /entrar/i });
+    expect(inputButtonEl).toBeInTheDocument();
+  });
+  test('Ao clicar no botão é direcionado para a aplicação', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    console.log('history', history);
+    const inputEmailEl = screen.getByTestId('email-input');
+    const inputPasswordEl = screen.getByTestId('password-input');
+    const inputButtonEl = screen.getByRole('button', { name: /entrar/i });
+    userEvent.type(inputEmailEl, 'alguem@mail.com');
+    userEvent.type(inputPasswordEl, '123456');
+    expect(inputButtonEl).toBeEnabled();
+    userEvent.click(inputButtonEl);
+    expect(history.location.pathname).toBe('/carteira');
+  });
+});
---
+++ src/tests/wallet.test.js
@@ -0,0 +1,20 @@
+// import { screen } from '@testing-library/react';
+// import App from '../App';
+// import { renderWithRouterAndRedux } from './helpers/renderWith';
+
+// describe('Verificar se na página wallet...', () => {
+//   test('Tem um input de valor com data testid = value-input', () => {
+//     renderWithRouterAndRedux(<App />, '/carteira');
+//     const inputValueEl = screen.getByTestId('value-input');
+//     expect(inputValueEl).toBeInTheDocument();
+//   });
+//   test('Tem um input de valor com data testid = value-input', () => {
+//     renderWithRouterAndRedux(<App />, '/carteira');
+//     const inputValueEl = test('Tem um input de valor com data testid = value-input', () => {
+//       renderWithRouterAndRedux(<App />, '/carteira');
+//       const valueName = screen.getByTestId('value-input');
+//       expect(valueName).toBeInTheDocument();
+//     });
+//     expect(inputValueEl).toBeInTheDocument();
+//   });
+// });
