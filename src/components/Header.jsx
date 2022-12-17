edf31696d1ca8db56df80195a61a56eef302d70b
hildelio <hildelio@gmail.com>
2022-12-17 12:05:08 -0300

    Componente Header estilizado :paintbrush: :art:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import '../css/header.css';
+import logo from '../assets/logo.png';
+import coinIcon from '../assets/coinIcon.png';
+import emailIcon from '../assets/emailIcon.png';
 
 class Header extends Component {
   render() {
@@ -8,20 +12,32 @@ class Header extends Component {
 
     return (
 
-      <div>
-        <p data-testid="email-field">{userEmail}</p>
-        <p data-testid="total-field">
-          {expenses.length !== 0 ? expenses.reduce((sum, e) => {
-            const { currency } = e;
-            const calcExpenses = e.value * e.exchangeRates[currency].ask;
-            const result = calcExpenses + sum;
-            return result;
-          }, 0).toFixed(2) : (0).toFixed(2)}
-        </p>
-        <p data-testid="header-currency-field">
-          BRL
-        </p>
-      </div>
+      <header className="header-container">
+        <img
+          src={ logo }
+          alt="logo"
+          className="header-image"
+        />
+        <section className="header-total-container">
+          <img src={ coinIcon } alt="icon" className="header-total-icon" />
+          <p className="header-total-text">Total de despesas:</p>
+          <p data-testid="total-field" className="header-total">
+            {expenses.length !== 0 ? expenses.reduce((sum, e) => {
+              const { currency } = e;
+              const calcExpenses = e.value * e.exchangeRates[currency].ask;
+              const result = calcExpenses + sum;
+              return result;
+            }, 0).toFixed(2) : (0).toFixed(2)}
+          </p>
+          <p data-testid="header-currency-field" className="header-total-brl">
+            BRL
+          </p>
+        </section>
+        <section className="header-email-container">
+          <img src={ emailIcon } alt="email-icon" className="header-email-icon" />
+          <p data-testid="email-field">{userEmail}</p>
+        </section>
+      </header>
     );
   }
 }
