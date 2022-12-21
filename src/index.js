69276b635a29813318a2cf772376ad48963670b0
hildelio <hildelio@gmail.com>
2022-12-20 23:09:08 -0300

    gh pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
+    <BrowserRouter basename="/myTrybewallet">
       <Provider store={ store }>
         <App />
       </Provider>
